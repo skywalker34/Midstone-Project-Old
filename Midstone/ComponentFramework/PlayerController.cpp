776d1d67cc1b9f38eb4cfc46695d1a172b49b87d
ATLenormand <bionicemperor8@gmail.com>
2024-09-23 17:09:03 -0400

    3D clicking basic implementation
    3D clicking via line-plane intersection (using dual quaternions), theres a bit more that has to be done so that it updates when the camera rotates.
    
    coordinate system seems to be wonky as I think the values are too small

--- Midstone/ComponentFramework/PlayerController.cpp
+++ Midstone/ComponentFramework/PlayerController.cpp
@@ -1,11 +1,16 @@
 #include "PlayerController.h"
 #include <SDL.h>
-#include "QMath.h"	
+
+#include <DQMath.h>
+#include <Plane.h>
+
+
 
 bool PlayerController::OnCreate()
 {
 	//here for future improvements
 	transform.setPos(Vec3(0, 0, -10));
+	camera.SetView(transform);
 	return true;
 	
 }
@@ -28,7 +33,7 @@ void PlayerController::handleEvents(const SDL_Event& sdlEvent)
 			//	below works *marginally* better (still jank af)
 			v = transform.getPos();
 			v += (-direction * speed);
-			
+		
 			transform.setPos(v);
 			break;
 
@@ -42,20 +47,68 @@ void PlayerController::handleEvents(const SDL_Event& sdlEvent)
 			transform.setPos(v);
 			break;
 
-		//future for strafing movement
+			//future for strafing movement
 		case SDL_SCANCODE_A:
-			
+
 			break;
 
 		case SDL_SCANCODE_D:
-			
+
 			break;
 
 
 
 		}
+		break;
+
+	case SDL_MOUSEBUTTONDOWN:
+
+	{
+		IHave3DClick = true;
+		Vec4 mousePosPixelSpace = Vec4(sdlEvent.button.x, sdlEvent.button.y, 0, 1);
+
+		Vec4 mousePosNDCSpace = MMath::inverse(MMath::viewportNDC(1280, 720)) * mousePosPixelSpace;
+		// Let's get the front of the NDC box
+		mousePosNDCSpace.z = -1.0f;
+
+		Vec4 mousePosCameraSpace = MMath::inverse(camera.GetProjectionMatrix()) * mousePosNDCSpace;
+		// Divide out the w component
+		mousePosCameraSpace = mousePosCameraSpace / mousePosCameraSpace.w;
+
+		Vec4 mousePosWorldSpace = MMath::inverse(MATHEX::DQMath::toMatrix4(camera.GetViewDQuaternion())) * mousePosCameraSpace;
+
+		// Make a line from the camera position to the mouse
+		// Using the join of two points
+		DualQuat line = transform.getPos() & mousePosWorldSpace;
+
+		// Make a plane based off scrollwheel
+		Plane plane = Plane(0, 0, 1, -(-planeDepth));
+
+		// intersection point is the meet of line and plane
+		Vec4 intersection = line ^ plane;
+
+		// Divide out the w component
+		intersection = intersection / intersection.w;
+
+
+		intersection.print("3D click at: ");
+	}
+	break;
+
+	case SDL_MOUSEBUTTONUP:
+		break;
+
+	case SDL_MOUSEWHEEL:
+		planeDepth += sdlEvent.wheel.preciseY;
+		//plane depth increases if the mouse is wheeled up (by the amount the mouse is wheeled)
+		//plane depth decreases if the mouse is wheeled down (by the amount the mouse is wheeled)
+
+
+		break;
+
+		
 	}
 
 	camera.SetView(transform);
-	
+
 }
--- Midstone/ComponentFramework/PlayerController.h
+++ Midstone/ComponentFramework/PlayerController.h
@@ -2,7 +2,7 @@
 #include "Camera.h"
 #include "Trackball.h"
 #include "Transform.h"
-
+#include <QMath.h>
 using namespace MATH;
 
 union SDL_Event;
@@ -15,11 +15,13 @@ class PlayerController
 		
 		Trackball trackball;
 		Transform transform;
-		Vec3 forward = Vec3(0.0f,0.0f,-1.0f); //tells teh program that into teh screen is the negative z axis (might be a better way to do this)
+		Vec3 forward = Vec3(0.0f,0.0f,-1.0f); //tells the program that into the screen is the negative z axis (might be a better way to do this)
 		float speed = 0.5f; //the rate of speed the player can move the camera
+		float planeDepth = 0;
 
 	public:
 		Camera camera;
+		bool IHave3DClick;
 		bool OnCreate();
 		void OnDestroy();
 		void handleEvents(const SDL_Event& sdlEvent);
